70fb2af05ea5e8eff49fe4f34bc87465bfabd979
ImArnavRana <arnavrana0704@gmail.com>
2026-03-05 00:17:58 +0530

    UC2: Hardcoded palindrome

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Palindrome {
+public class Palindrome{
 
     public static void main(String[] args) {
 
@@ -8,17 +8,25 @@ public class Palindrome {
 
         System.out.println("Welcome to Palindrome Checker App");
         System.out.print("Enter a string: ");
-        String original = scanner.nextLine();
+        String input = scanner.nextLine();
 
-        String reversed = "";
+        char[] arr = input.toCharArray();
 
-        // Reverse string using for loop
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+        int start = 0;
+        int end = arr.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (arr[start] != arr[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        // Compare original and reversed string
-        if (original.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println("It is a Palindrome.");
         } else {
             System.out.println("It is NOT a Palindrome.");
