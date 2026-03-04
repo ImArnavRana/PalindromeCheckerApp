ccdb4c7d036b487dc9c9180cbfea484c20327c39
ImArnavRana <arnavrana0704@gmail.com>
2026-03-04 23:37:28 +0530

    UC2: Hardcoded palindrome

---
+++ src/Palindrome.java
@@ -0,0 +1,17 @@
+public class Palindrome {
+    public static void main(String[] args) {
+
+        String word = "madam";
+        String reversed = "";
+
+        for(int i = word.length()-1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        if(word.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
